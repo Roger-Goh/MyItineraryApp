bcee1b815a1281d1fc31f56a8e27ba5907c8f870
Roga <rogergoh@outlook.my>
2018-05-20 02:13:51 +0800

    my first page creation test

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
+var homeRouter = require('./app_server/routes/home'); //mine :)
+
 var app = express();
 
 // view engine setup
@@ -19,8 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter); //when no sub path is provided
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
