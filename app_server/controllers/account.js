415c4761d18d7b80c03c23d8b2e88ab72732f1b9
Roga <rogergoh@outlook.my>
2018-05-28 17:27:15 +0800

    create user data when new account is registered

--- app_server/controllers/account.js
+++ app_server/controllers/account.js
@@ -1,8 +1,13 @@
 var mongoose = require("mongoose");
 require("../models/db")
-var Account = require("../models/account");
+require("../models/holiday_model");
 
+//handling accounts
+var Account = require("../models/account");
 var passport = require('passport');
+
+//adding a new holiday list for when a new account is made
+var Holiday = mongoose.model('Holiday');
 
 //var Account = mongoose.model('Account');
 
@@ -20,10 +25,55 @@ module.exports.regist = function(req, res) {
                    return res.render('register', { msg : 'rego failed'});
                  }
                  passport.authenticate('local')(req, res, function () {
-                   res.redirect('/createPlan');
-
+                     console.log('Created new account');
+                        createUser(req, res);
+                   //res.redirect('/createPlan'); //maybe comment out and put in createUser
                    });
                });
+};
+
+//when regist creates a new account, creates a new holidayList for them to load in view
+function createUser(req, res){
+    var newUser = new Holiday({
+        "userName": req.body.username, //maybe req.user.username instead
+        "nHolidays": 0,
+        "holiday1": {"hasEntry": "0",
+            "dest": "Sample",
+            "year": 2019,
+            "desc": "Example description here",
+            "nTasks": 0,
+            "tasks": []
+        },
+        "holiday2": {
+            "hasEntry": "0",
+            "dest": "Sample",
+            "year": 2020,
+            "desc": "Example description here",
+            "nTasks": 0,
+            "tasks": []
+        },
+        "holiday3": {
+            "hasEntry": "0",
+            "dest": "Sample",
+            "year": 2021,
+            "desc": "Example description here",
+            "nTasks": 0,
+            "tasks": []
+        }
+    });
+    Holiday.create(newUser, function(err, data){ //.create is the model equivalent to collection.insert()
+        if(err){
+            console.log(err);
+            res.status(500);
+            res.render('error',{
+                message:err.message,
+                error:err
+            });
+        }else{
+            console.log('Created user data for new account');
+            res.redirect('/createPlan');
+        }
+      });
 };
 
 module.exports.loginForm = function(req, res) {
